0209dca8d26f38c9dfae711a057e2664d2abf8e1
daothanhhuy <19521601@gm.uit.edu.vn>
2021-11-06 22:29:21 +0700

    LoginActivity: phat hien sai dinh dang email, them progress dialog

--- ProjectLandView/app/src/main/java/com/example/landview/LoginActivity.java
+++ ProjectLandView/app/src/main/java/com/example/landview/LoginActivity.java
@@ -1,19 +1,32 @@
 package com.example.landview;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,15 +37,18 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
-    private static final String TAG ="LoginActivity";
+import java.util.regex.Pattern;
 
-    private EditText editTextemailLogin,editTextpassLogin;
-    private TextView txtForgot,txtSignupnow;
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
+    private EditText editTextemailLogin, editTextpassLogin;
+    private TextView txtForgot, txtSignupnow;
     private Button btnLogin;
-    private ImageView fbImage,ggImage;
+    private ImageView fbImage, ggImage;
     SharedPreferences sharedPreferences;
     private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
@@ -50,10 +66,10 @@ public class LoginActivity extends AppCompatActivity {
         //gọi instance của class FirebaseAuth
         mAuth = FirebaseAuth.getInstance();
 
-        sharedPreferences = getSharedPreferences("MODE_LOGIN",MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences("MODE_LOGIN", MODE_PRIVATE);
         //lấy ra
-        editTextemailLogin.setText(sharedPreferences.getString("Email",""));
-        editTextpassLogin.setText(sharedPreferences.getString("Password",""));
+        editTextemailLogin.setText(sharedPreferences.getString("Email", ""));
+        editTextpassLogin.setText(sharedPreferences.getString("Password", ""));
 
         //thay đổi label action bar
 //        getSupportActionBar().setTitle("Login");
@@ -65,11 +81,12 @@ public class LoginActivity extends AppCompatActivity {
 //                CheckData();
 //            }
 //        });
+
         //sự kiện textview sign up now
         txtSignupnow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -78,7 +95,7 @@ public class LoginActivity extends AppCompatActivity {
         txtForgot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,ForgotPassActivity.class);
+                Intent intent = new Intent(LoginActivity.this, ForgotPassActivity.class);
                 startActivity(intent);
             }
         });
@@ -89,74 +106,80 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String email = editTextemailLogin.getText().toString().trim();
                 String password = editTextpassLogin.getText().toString();
-                if(CheckData(email, password)){
+                if (CheckData(email, password)) {
                     login(email, password);
                 }
             }
         });
+
+        // Khởi tạo progress dialog
+        // Tạm thời xài cái này, trong tương lai xài ProgressBar
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+
+
     }
 
     private void login(String email, String password) {
-        if(TextUtils.isEmpty(email)){
-            editTextemailLogin.setError("Email section can't be empty");
-            editTextemailLogin.requestFocus();
-        }else if (TextUtils.isEmpty(password)){
-            editTextpassLogin.setError("Password section can't be empty");
-            editTextpassLogin.requestFocus();
-        }else{
-            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_LONG).show();
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        startActivity(intent);
+        progressDialog.show(); // gọi progressDialog
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.dismiss(); // Tắt progressDialog
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
 
-                    }else{
-                        Log.d(TAG, "onComplete: fail to login");
+                } else {
+                    progressDialog.dismiss(); // Tắt progressDialog
+                    Log.d(TAG, "onComplete: fail to login");
 
-                        // throw ra các exception
-                        try {
-                            throw task.getException();
+                    // throw ra các exception
+                    try {
+                        throw task.getException();
+                    }
+                    // Bắt exception liên quan mạng
+                    catch (FirebaseNetworkException networkException) {
+
+                        Toast.makeText(LoginActivity.this, "Không có kết nối mạng", Toast.LENGTH_SHORT).show();
+                    }
+                    // Bắt exception liên quan xác thực
+                    catch (FirebaseAuthException firebaseAuth) {
+                        // Lấy mã lỗi
+
+                        String errorCode = firebaseAuth.getErrorCode();
+                        switch (errorCode) {
+
+                            // Sai mật khẩu
+                            case "ERROR_WRONG_PASSWORD":
+                                Toast.makeText(LoginActivity.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                                editTextpassLogin.setError("Wrong password");
+                                editTextpassLogin.requestFocus();
+                                break;
+
+                            // Tài khoản ko tồn tại
+                            case "ERROR_USER_NOT_FOUND":
+                                Log.d(TAG, "onComplete: Not found user");
+                                Toast.makeText(LoginActivity.this, "Không tìm thấy tài khoản", Toast.LENGTH_SHORT).show();
+                                editTextemailLogin.setError("User not found");
+                                editTextemailLogin.requestFocus();
+                                break;
+
+                            // Lỗi khác liên quan đến đăng nhập
+                            default:
+                                Toast.makeText(LoginActivity.this, "Error: " + errorCode, Toast.LENGTH_SHORT).show();
                         }
-                        // Bắt exception liên quan mạng
-                        catch (FirebaseNetworkException networkException) {
-                            Toast.makeText(LoginActivity.this, "Không có kết nối mạng", Toast.LENGTH_SHORT).show();
-                        }
-                        // Bắt exception liên quan xác thực
-                        catch (FirebaseAuthException firebaseAuth) {
-                            // Lấy mã lỗi
-                            String errorCode = firebaseAuth.getErrorCode();
-                            switch (errorCode) {
+                        // Lỗi khác
+                    } catch (Exception e) {
 
-                                // Sai mật khẩu
-                                case "ERROR_WRONG_PASSWORD":
-                                    Toast.makeText(LoginActivity.this, "Wrong password", Toast.LENGTH_SHORT).show();
-                                    editTextpassLogin.setError("Wrong password");
-                                    editTextpassLogin.requestFocus();
-                                    break;
-
-                                // Tài khoản ko tồn tại
-                                case "ERROR_USER_NOT_FOUND":
-                                    Log.d(TAG, "onComplete: Not found user");
-                                    Toast.makeText(LoginActivity.this, "Không tìm thấy tài khoản", Toast.LENGTH_SHORT).show();
-                                    editTextemailLogin.setError("User not found");
-                                    editTextemailLogin.requestFocus();
-                                    break;
-
-                                // Lỗi khác liên quan đến đăng nhập
-                                default:
-                                    Toast.makeText(LoginActivity.this, "Error: " + errorCode, Toast.LENGTH_SHORT).show();
-                            }
-                            // Lỗi khác
-                        } catch (Exception e) {
-                            Toast.makeText(LoginActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
+                        Toast.makeText(LoginActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 }
-            });
-        }
+            }
+        });
     }
 
     private boolean CheckData(String email, String password) {
@@ -176,11 +199,15 @@ public class LoginActivity extends AppCompatActivity {
 //            editor.putString("Password",textPass);
 //            editor.apply();
 //        }
-        if(TextUtils.isEmpty(email)){
+        if (TextUtils.isEmpty(email)) {
             editTextemailLogin.setError("Email section can't be empty");
             editTextemailLogin.requestFocus();
             return false;
-        }else if(TextUtils.isEmpty(password)){
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) { // Nếu ko đụng định dạng email
+            editTextemailLogin.setError("Bad form email address");
+            editTextemailLogin.requestFocus();
+            return false;
+        } else if (TextUtils.isEmpty(password)) {
             editTextpassLogin.setError("Password section can't be empty");
             editTextpassLogin.requestFocus();
             return false;
