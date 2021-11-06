d1c67092ecc5d82f138457d70ce0e74e01caa3e6
daothanhhuy <19521601@gm.uit.edu.vn>
2021-11-06 20:01:54 +0700

    LoginActivity: Phat hien sai mk, tk ko ton tai, ko co internet

--- ProjectLandView/app/src/main/java/com/example/landview/LoginActivity.java
+++ ProjectLandView/app/src/main/java/com/example/landview/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,11 +18,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG ="LoginActivity";
+
     private EditText editTextemailLogin,editTextpassLogin;
     private TextView txtForgot,txtSignupnow;
     private Button btnLogin;
@@ -82,17 +87,16 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(CheckData()){
-                    login();
+                String email = editTextemailLogin.getText().toString().trim();
+                String password = editTextpassLogin.getText().toString();
+                if(CheckData(email, password)){
+                    login(email, password);
                 }
             }
         });
     }
 
-    private void login() {
-        String email = editTextemailLogin.getText().toString();
-        String password = editTextpassLogin.getText().toString();
-
+    private void login(String email, String password) {
         if(TextUtils.isEmpty(email)){
             editTextemailLogin.setError("Email section can't be empty");
             editTextemailLogin.requestFocus();
@@ -110,17 +114,53 @@ public class LoginActivity extends AppCompatActivity {
                         startActivity(intent);
 
                     }else{
-                        Toast.makeText(LoginActivity.this, "Invalid Credential", Toast.LENGTH_LONG).show();
+                        Log.d(TAG, "onComplete: fail to login");
+
+                        // throw ra các exception
+                        try {
+                            throw task.getException();
+                        }
+                        // Bắt exception liên quan mạng
+                        catch (FirebaseNetworkException networkException) {
+                            Toast.makeText(LoginActivity.this, "Không có kết nối mạng", Toast.LENGTH_SHORT).show();
+                        }
+                        // Bắt exception liên quan xác thực
+                        catch (FirebaseAuthException firebaseAuth) {
+                            // Lấy mã lỗi
+                            String errorCode = firebaseAuth.getErrorCode();
+                            switch (errorCode) {
+
+                                // Sai mật khẩu
+                                case "ERROR_WRONG_PASSWORD":
+                                    Toast.makeText(LoginActivity.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                                    editTextpassLogin.setError("Wrong password");
+                                    editTextpassLogin.requestFocus();
+                                    break;
+
+                                // Tài khoản ko tồn tại
+                                case "ERROR_USER_NOT_FOUND":
+                                    Log.d(TAG, "onComplete: Not found user");
+                                    Toast.makeText(LoginActivity.this, "Không tìm thấy tài khoản", Toast.LENGTH_SHORT).show();
+                                    editTextemailLogin.setError("User not found");
+                                    editTextemailLogin.requestFocus();
+                                    break;
+
+                                // Lỗi khác liên quan đến đăng nhập
+                                default:
+                                    Toast.makeText(LoginActivity.this, "Error: " + errorCode, Toast.LENGTH_SHORT).show();
+                            }
+                            // Lỗi khác
+                        } catch (Exception e) {
+                            Toast.makeText(LoginActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
             });
         }
     }
 
-    private boolean CheckData() {
-        //kiểm tra input người dùng nhập vào
-        String textEmail = editTextemailLogin.getText().toString().trim();
-        String textPass = editTextpassLogin.getText().toString();
+    private boolean CheckData(String email, String password) {
+
 //        if(textEmail.length()==0 || textPass.length() ==0)
 //        {
 //            Toast.makeText(this,"Login failed",Toast.LENGTH_LONG).show();
@@ -136,11 +176,11 @@ public class LoginActivity extends AppCompatActivity {
 //            editor.putString("Password",textPass);
 //            editor.apply();
 //        }
-        if(TextUtils.isEmpty(textEmail)){
+        if(TextUtils.isEmpty(email)){
             editTextemailLogin.setError("Email section can't be empty");
             editTextemailLogin.requestFocus();
             return false;
-        }else if(TextUtils.isEmpty(textPass)){
+        }else if(TextUtils.isEmpty(password)){
             editTextpassLogin.setError("Password section can't be empty");
             editTextpassLogin.requestFocus();
             return false;
